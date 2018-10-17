fcf5092b219e926508ac25413afebfa446416620
TheOptimime <jwjaw8@gmail.com>
2018-10-17 19:16:56 -0400

    Now with CSV support to be implemented

---
+++ Assets/Scripts/CSVToAttack.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class CSVToAttack : MonoBehaviour {
+
+    
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        FileStream fs;
+        //File.AppendAllText("/Assets/Data" + "/Assets/Data/AttackData.csv", "," + rollNoInputField.text + fieldSeperator + nameInputField.text);
+
+    }
+}
