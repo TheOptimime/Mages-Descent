0b032914e30ae0983c37847aedb91e70189a3b3e
kupoo <43799132+kupoo@users.noreply.github.com>
2019-04-16 22:10:22 -0400

    push of stuff

---
+++ Assets/Scripts/Misc/TestBeam.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestBeam : MonoBehaviour
+{
+
+	public Transform spawner; 
+	public GameObject particleToSpawn;
+
+	public float speed; 
+	float timer; 
+    // Start is called before the first frame update
+    void Start()
+    {
+		timer = .01f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		timer -= Time.deltaTime;
+		if (timer <= 0) {
+			Invoke ("SpawnParticle", 0);
+			timer = .01f;
+		
+		}
+
+
+	
+    }
+
+	public void SpawnParticle() {
+	
+	
+		Instantiate (particleToSpawn, spawner.position, spawner.rotation);
+
+	}
+}
--- Assets/Scripts/Misc/destroyParticle.cs
+++ Assets/Scripts/Misc/destroyParticle.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class destroyParticle : MonoBehaviour
 {
+
+	public float destroyTimer;
     // Start is called before the first frame update
     void Start()
     {
-		Destroy (gameObject, 3f);
+		Destroy (gameObject, destroyTimer);
     }
 
 
