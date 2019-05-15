af3e6cdb40f83b5587bf3d856d10f78a91d55b32
TheOptimime <jwjaw8@gmail.com>
2019-05-15 08:20:00 -0400

    Update GenericDamage.cs

--- Assets/GenericDamage.cs
+++ Assets/GenericDamage.cs
@@ -4,9 +4,15 @@ using UnityEngine;
 
 public class GenericDamage : MonoBehaviour
 {
+    public EnemyAI enemy;
     public int damage;
     public Vector2 knockback;
     public DoubleTime hitstun;
+
+    private void Start()
+    {
+        if (enemy == null) enemy = gameObject.gameObject.GetComponent<EnemyAI>();
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -16,6 +22,7 @@ public class GenericDamage : MonoBehaviour
             
             collision.gameObject.GetComponent<Health>().Damage(damage);
             collision.gameObject.GetComponent<Fighter>().recentlyAttacked = true;
+            if(enemy) enemy.hitTarget = true;
             //print(collision.gameObject.GetComponent<Health>().currentHealth);
             KnockbackListener kbl = collision.gameObject.GetComponent<KnockbackListener>();
 
